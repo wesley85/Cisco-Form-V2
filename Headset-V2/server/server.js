447627b907611bea13dcc3e4a217212af059caf0
Wesley Seitz <wseitz@west.com>
2019-11-25 08:56:16 -0700

    updated server files

--- Headset-V2/server/server.js
+++ Headset-V2/server/server.js
@@ -5,7 +5,6 @@ const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
-const http = require('http');
 
 app.use(express.static(publicPath));
 
@@ -13,46 +12,88 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'));
 });
 
+app.listen(port, () => {
+    console.log('Server is running');
+    console.log(`Server listening on port ${port}`)
+});
+
+// Below is for axios and mail sender
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-
-  const fromEmail = 'Wesley Seitz';
-  const toEmail = 'wseitz@west.com';
-
-  const transporter = nodemailer.createTransport({
-    host: 'smtp.icallinc.com',
-    port: 25
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
   });
-   transporter.sendMail({
-    from: fromEmail, // sender address
-    to: toEmail, // list of receivers
-    subject: 'Cisco Headset Order Form', // Subject line
-    // text: 'change to what you would like to use', // plain text body
-    html: `
-    <h3>Cisco Headset Order Form</h3>
-        <p>First Name: ${req.body.fname}<br/>
-        Last Name: ${req.body.lname}<br/>
-        Email: ${req.body.email}<br/>
-        Phone: ${req.body.phone}<br/>
-        PO Number: ${req.body.ponum}<br/>
-        Headset 531: ${req.body.prod531}<br/>
-        Headset 532: ${req.body.prod532}<br/>
-        Headset 561 Standard Base: ${req.body.prod561sb}<br/>
-        Headset 561 Multi Base: ${req.body.prod561mb}<br/>
-        Headset 562 Standard Base: ${req.body.prod562sb}<br/>
-        Headset 562 Multi Base: ${req.body.prod562mb}<br/>
-        Total Count: ${req.body.totalcount}</p>
-    <p>notes:<br/>${req.body.note}</p>
-    ` // html body
-  });
-  const response = {}
-  response.status = 200
-  if (response.status === 200) {
-    actions.setStatus( {success: 'Email sent !'} )
-    actions.resetForm()
-  }
-}).listen(port);
-console.log(`Server listening on port ${port}`);
+
+app.post('/wes_test/', (req, res) => {
+        // async..await is not allowed in global scope, must use a wrapper
+async function main() {
+    // create reusable transporter object using the default SMTP transport
+    // let transporter = nodemailer.createTransport({
+    //     host: 'smtp.icallinc.com',
+    //     port: 25
+    // });
+
+    var mailConfig;
+if (process.env.NODE_ENV === 'production' ){
+    // all emails are delivered to destination
+    mailConfig = {
+        host: 'smtp.icallinc.com',
+        port: 25,
+        auth: {
+            user: 'wseitz@west.com'
+        }
+    };
+} else {
+    // all emails are catched by ethereal.email
+    mailConfig = {
+        host: 'smtp.ethereal.email',
+        port: 587,
+        auth: {
+            user: 'tke5ixparklmznda@ethereal.email',
+            pass: '5wbgKC9BG2U32PDJcr'
+        }
+    };
+}
+let transporter = nodemailer.createTransport(mailConfig);
+
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+        from: '"Wesley Seitz" <wseitz@west.com>', // sender address
+        to: 'wseitz@west.com', // list of receivers
+        subject: 'Cisco Headset Order Form', // Subject line
+        // text: req.body.message, // plain text body
+        html: `
+        <h3>Cisco Headset Order Form</h3>
+            <p>First Name: ${req.body.fname}<br/>
+            Last Name: ${req.body.lname}<br/>
+            Email: ${req.body.email}<br/>
+            Phone: ${req.body.phone}<br/>
+            PO Number: ${req.body.ponum}<br/>
+            Headset 531: ${req.body.prod531}<br/>
+            Headset 532: ${req.body.prod532}<br/>
+            Headset 561 standard Base: ${req.body.prod561sb}<br/>
+            Headset 561 Multi Base: ${req.body.prod561mb}<br/>
+            Headset : ${req.body.prod562sb}<br/>
+            Headset : ${req.body.prod562mb}<br/>
+            Total Count: ${req.body.totalcount}</p>
+        <p>notes:<br/>${req.body.note}</p>
+        ` // html body
+    },
+     function(err){
+        if(err) {
+            console.log(err);
+        } else {
+            console.log('Email sent: ' + info.response);
+          }
+    });
+
+    console.log('Message sent: %s', info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+}
+
+main().catch(console.error);
+
+    })
